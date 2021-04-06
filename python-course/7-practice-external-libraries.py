cca0c160e438ce12882d88169068d86e919bc94a
André Klingenfus Antunes <andrekantunes10@gmail.com>
2021-04-06 20:58:10 -0300

    added lesson 7 libraries

---
+++ python-course/7-practice-external-libraries.py
@@ -0,0 +1,111 @@
+# Practice 7 - External libraries
+
+# Question 1 -
+
+# After completing the exercises on lists and tuples, Jimmy noticed that, according to his estimate_average_slot_payout function,
+# the slot machines at the Learn Python Casino are actually rigged against the house, and are profitable to play in the long run.
+
+# Starting with $200 in his pocket, Jimmy has played the slots 500 times, recording his new balance in a list after each spin.
+# He used Python's matplotlib library to make a graph of his balance over time:
+
+# Import the jimmy_slots submodule
+from learntools.python import jimmy_slots
+
+# Call the get_graph() function to get Jimmy's graph
+graph = jimmy_slots.get_graph()
+graph
+
+def prettify_graph(graph):
+    graph.set_title("Results of 500 slot machine pulls")
+    # Make the y-axis begin at 0
+    graph.set_ylim(bottom=0)
+    # Label the y-axis
+    graph.set_ylabel("Balance")
+    # Bonus: format the numbers on the y-axis as dollar amounts
+    # An array of the values displayed on the y-axis (150, 175, 200, etc.)
+    ticks = graph.get_yticks()
+    # Format those values into strings beginning with dollar sign
+    new_labels = ['${}'.format(int(amt)) for amt in ticks]
+    # Set the new labels
+    graph.set_yticklabels(new_labels)
+
+graph = jimmy_slots.get_graph()
+prettify_graph(graph)
+graph
+
+# Question 2 -
+
+# Luigi is trying to perform an analysis to determine the best items for winning races on the Mario Kart circuit.
+# He has some data in the form of lists of dictionaries that look like
+
+[
+    {'name': 'Peach', 'items': ['green shell', 'banana', 'green shell',], 'finish': 3},
+    {'name': 'Bowser', 'items': ['green shell',], 'finish': 1},
+    # Sometimes the racer's name wasn't recorded
+    {'name': None, 'items': ['mushroom',], 'finish': 2},
+    {'name': 'Toad', 'items': ['green shell', 'mushroom'], 'finish': 1},
+]
+
+def best_items(racers):
+    """Given a list of racer dictionaries, return a dictionary mapping items to the number
+    of times those items were picked up by racers who finished in first place.
+    """
+    winner_item_counts = {}
+    for i in range(len(racers)):
+        # The i'th racer dictionary
+        racer = racers[i]
+        # We're only interested in racers who finished in first
+        if racer['finish'] == 1:
+            for i in racer['items']:
+                # Add one to the count for this item (adding it to the dict if necessary)
+                if i not in winner_item_counts:
+                    winner_item_counts[i] = 0
+                winner_item_counts[i] += 1
+
+        # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
+        if racer['name'] is None:
+            print("WARNING: Encountered racer with unknown name on iteration {}/{} (racer = {})".format(
+                i+1, len(racers), racer['name'])
+                 )
+    return winner_item_counts
+
+sample = [
+    {'name': 'Peach', 'items': ['green shell', 'banana', 'green shell',], 'finish': 3},
+    {'name': 'Bowser', 'items': ['green shell',], 'finish': 1},
+    {'name': None, 'items': ['mushroom',], 'finish': 2},
+    {'name': 'Toad', 'items': ['green shell', 'mushroom'], 'finish': 1},
+]
+best_items(sample)
+
+### Question 3 - 
+
+def hand_total(hand):
+    """Helper function to calculate the total points of a blackjack hand.
+    """
+    total = 0
+    # Count the number of aces and deal with how to apply them at the end.
+    aces = 0
+    for card in hand:
+        if card in ['J', 'Q', 'K']:
+            total += 10
+        elif card == 'A':
+            aces += 1
+        else:
+            # Convert number cards (e.g. '7') to ints
+            total += int(card)
+    # At this point, total is the sum of this hand's cards *not counting aces*.
+
+    # Add aces, counting them as 1 for now. This is the smallest total we can make from this hand
+    total += aces
+    # "Upgrade" aces from 1 to 11 as long as it helps us get closer to 21
+    # without busting
+    while total + 10 <= 21 and aces > 0:
+        # Upgrade an ace from 1 to 11
+        total += 10
+        aces -= 1
+    return total
+
+def blackjack_hand_greater_than(hand_1, hand_2):
+    total_1 = hand_total(hand_1)
+    total_2 = hand_total(hand_2)
+    return total_1 <= 21 and (total_1 > total_2 or total_2 > 21)
