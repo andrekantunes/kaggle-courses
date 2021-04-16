3630f6d1060bd2744d5770ec0571161005c7cefc
André Klingenfus Antunes <andrekantunes10@gmail.com>
2021-04-16 00:27:06 -0300

    added lesson 7 data leakage

--- 03-intermediate-machine-learning/07-lesson-data-leakage.py
+++ 03-intermediate-machine-learning/07-lesson-data-leakage.py
@@ -59,15 +59,4 @@ expenditures_noncardholders = X.expenditure[~y]
 print('Fraction of those who did not receive a card and had no expenditures: %.2f' \
       %((expenditures_noncardholders == 0).mean()))
 print('Fraction of those who received a card and had no expenditures: %.2f' \
-      %(( expenditures_cardholders == 0).mean()))
-
-# Drop leaky predictors from dataset
-potential_leaks = ['expenditure', 'share', 'active', 'majorcards']
-X2 = X.drop(potential_leaks, axis=1)
-
-# Evaluate the model with leaky predictors removed
-cv_scores = cross_val_score(my_pipeline, X2, y, 
-                            cv=5,
-                            scoring='accuracy')
-
-print("Cross-val accuracy: %f" % cv_scores.mean())      
+      %(( expenditures_cardholders == 0).mean()))      
